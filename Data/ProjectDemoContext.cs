b72f6cdb3c0275e224532e6471c6b2a294eddcd6
Nishchitha <nishushetty151997@gmail.com>
2020-01-31 17:16:05 +0530

    first commit

---
+++ Data/ProjectDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectDemo.Models;
+
+namespace ProjectDemo.Data
+{
+    public class ProjectDemoContext : DbContext
+    {
+        public ProjectDemoContext (DbContextOptions<ProjectDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectDemo.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProjectDemo.Data;
+using ProjectDemo.Models;
+
+namespace ProjectDemo
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ProjectDemo.Data.ProjectDemoContext _context;
+
+        public IndexModel(ProjectDemo.Data.ProjectDemoContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
